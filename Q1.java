033c08e0dbf2722201235d582f52fe5663f2dc03
brenokly <breno.moura@alunos.ufersa.edu.br>
2023-12-04 13:29:30 -0300

    Questões

---
+++ Q1.java
@@ -0,0 +1,13 @@
+public class Q1 {
+    public static void main(String[] args) throws Exception {
+        int idade = 19;
+        double altura = 1.85;
+        char pletra = 'B';
+        String nome = "Breno Klywer Olegario de Moura";
+        
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Letra: " + pletra);
+    }
+}
